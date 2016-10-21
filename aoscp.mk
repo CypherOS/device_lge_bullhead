be211ace43902959aca0c34d156b76afa5a27377
Chris <chriscrawf95@gmail.com>
2016-10-21 10:08:00 -0700

    Bringup the aoscp makefile

---
+++ aoscp.mk
@@ -0,0 +1,24 @@
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/aoscp/configs/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := aoscp_bullhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5X
+TARGET_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead \
+    BUILD_FINGERPRINT="google/bullhead/bullhead:7.0/NBD90W/3239497:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 7.0 NBD90W 3239497 release-keys"
