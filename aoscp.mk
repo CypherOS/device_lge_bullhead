d6a9d254c3dfdfb3338c1c1b9bf197266762600c
Chris <chris@cypheros.co>
2017-08-22 17:45:05 -0400

    bullhead: add aoscp build setup makefiles
    Change-Id: Iaf42c4abc8e730124b09de231d2e9e0dfefc4f46

---
+++ aoscp.mk
@@ -0,0 +1,19 @@
+# Inherit some common stuff.
+$(call inherit-product, vendor/aoscp/configs/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/lge/bullhead/aosp_bullhead.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := aoscp_bullhead
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 5X
+TARGET_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=bullhead
