906264b81c2259254d3022b7443f84a917b45988
argraur <argraur@gmail.com>
2018-08-09 22:19:10 +0000

    bullead: Drop Health HAL
    Bullhead does not support it
    
    Change-Id: If50c0bc0d4e1bdde142b5119112f472d9d822eb4
    Signed-off-by: argraur <argraur@gmail.com>

--- device.mk
+++ device.mk
@@ -296,10 +296,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     camera.device@3.2-impl
 
-# health HAL
-PRODUCT_PACKAGES += \
-	android.hardware.health@2.0-service.bullhead
-
 # Sensor & activity_recognition HAL
 TARGET_USES_NANOHUB_SENSORHAL := true
 NANOHUB_SENSORHAL_LID_STATE_ENABLED := true
